2108e684206d8193ef43f1f890586632b17be152
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2021-06-25 09:47:51 +1000

    Add App signal to test it

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
+const { Appsignal } = require("@appsignal/nodejs");
+const appsignal = new Appsignal({
+  active: true,
+  name: "quotes-api",
+  apiKey: "27aec3a5-ee1a-492b-b45d-64153e3d79ae",
+});
+
 var express = require('express');
+const { expressMiddleware, expressErrorHandler } = require("@appsignal/express");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +16,7 @@ var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
+app.use(expressMiddleware(appsignal));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
+
+app.use(expressErrorHandler(appsignal))
 
 module.exports = app;
